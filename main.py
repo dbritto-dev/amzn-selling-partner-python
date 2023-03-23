4485d9ce396b6ca305b4539040e70af71f95ed83
dbritto.dev <dbritto.dev@gmail.com>
2023-03-22 20:48:42 -0500

    Clean up files

--- main.py
+++
@@ -1,9 +0,0 @@
-import dotenv
-
-dotenv.load_dotenv()
-
-from amzn_selling_partner.vendor import orders as vendor_orders
-
-vendor_orders_client = vendor_orders.Client()
-
-print(vendor_orders_client.get_purchase_orders())
