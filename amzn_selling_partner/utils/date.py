e18055845175ee6593855f6c5a90405e57d4e292
dbritto.dev <dbritto.dev@gmail.com>
2023-03-24 08:01:45 -0500

    Update date utils

--- amzn_selling_partner/utils/date.py
+++ amzn_selling_partner/utils/date.py
@@ -6,10 +6,6 @@ def datetime_utcnow() -> datetime.datetime:
     return datetime.datetime.utcnow()
 
 
-def date_utcnow() -> datetime.date:
-    return datetime_utcnow().date()
-
-
 def datetime_utcpast(
     amount: typing.Union[int, float],
     amount_type: typing.Literal[
@@ -19,14 +15,5 @@ def datetime_utcpast(
     return datetime_utcnow() - datetime.timedelta(**{amount_type: amount})
 
 
-def date_utcpast(
-    amount: typing.Union[int, float],
-    amount_type: typing.Literal[
-        "weeks", "days", "hours", "minutes", "seconds", "milliseconds", "microseconds"
-    ],
-) -> datetime.date:
-    return datetime_utcpast(amount, amount_type).date()
-
-
-def amazon_isoformat(value: typing.Union[datetime.date, datetime.datetime]) -> str:
+def amazon_isoformat(value: datetime.datetime) -> str:
     return f"{value.isoformat(timespec='milliseconds')}Z"
