da87854bddcec346c2b18060fd83c5f517c6ee0e
dbritto.dev <dbritto.dev@gmail.com>
2023-03-24 22:51:55 -0500

    Update unit tests

--- tests/test_utils_date.py
+++ tests/test_utils_date.py
@@ -2,28 +2,31 @@ import datetime
 
 import pytest
 
-import amzn_selling_partner.utils.date as sp_date_utils
+import amzn_selling_partner as sp
 
 
 @pytest.fixture(autouse=True)
 def mock_datetime_utcnow(monkeypatch: pytest.MonkeyPatch) -> None:
-    mock_datetime = type(
-        "mockdatetime",
-        (datetime.datetime,),
-        {"utcnow": classmethod(lambda _: datetime.datetime(2023, 1, 1))},
+    monkeypatch.setattr(
+        datetime,
+        "datetime",
+        type(
+            "mockdatetime",
+            (datetime.datetime,),
+            {"utcnow": classmethod(lambda _: datetime.datetime(2023, 1, 1))},
+        ),
     )
-    monkeypatch.setattr(datetime, "datetime", mock_datetime)
 
 
 def test_datetime_utcnow() -> None:
-    assert "2023-01-01T00:00:00" == sp_date_utils.datetime_utcnow().isoformat()
+    assert "2023-01-01T00:00:00" == sp.utils.date.datetime_utcnow().isoformat()
 
 
 def test_datetime_utcpast() -> None:
-    assert "2022-12-02T00:00:00" == sp_date_utils.datetime_utcpast(30, "days").isoformat()
+    assert "2022-12-02T00:00:00" == sp.utils.date.datetime_utcpast(30, "days").isoformat()
 
 
 def test_amazon_isoformat() -> None:
-    assert "2023-01-01T00:00:00.000Z" == sp_date_utils.amazon_isoformat(
-        sp_date_utils.datetime_utcnow()
+    assert "2023-01-01T00:00:00.000Z" == sp.utils.date.amazon_isoformat(
+        sp.utils.date.datetime_utcnow()
     )
--- tests/test_utils_file.py
+++ tests/test_utils_file.py
@@ -1,11 +1,11 @@
-import amzn_selling_partner.utils.file as sp_file_utils
+import amzn_selling_partner as sp
 
 
 def test_write_binary_file_(tmp_path):
     file_path = tmp_path / "testing.json"
     content = b"{}"
 
-    sp_file_utils.write_binary_file(file_path, content)
+    sp.utils.file.write_binary_file(file_path, content)
 
     with open(file_path) as f:
         assert content.decode() == f.read()
